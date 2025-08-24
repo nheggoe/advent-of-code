c4cf812f756aca2a324cd55ec392c1bf7008d97c
Nick Heggø <nickhe@stud.ntnu.no>
2025-08-24 22:34:14 +0200

    refactor: simplify Day3 solution and update tests for clarity
    - Replaced `Memory` class with simplified `partOne` and `partTwo` functions.
    - Optimized regex usage for efficiency.
    - Updated tests to align with the refactored methods.

--- scala/src/main/scala/aoc24/Day3.scala
+++ scala/src/main/scala/aoc24/Day3.scala
@@ -7,47 +7,32 @@ import scala.util.matching.Regex
 
 /** @see `https://adventofcode.com/2024/day/3` */
 object Day3 {
-  val input: String = InputFetcher.fetchInput(2024, 3)
+  private val aocInput: String =
+    InputFetcher.fetchInput(2024, 3)
+  private val mulPattern: Regex = """mul\((\d+),(\d+)\)""".r
+  private val allPattern: Regex =
+    """(mul\((\d+),(\d+)\)|do\(\)|don't\(\))""".r
 
-  case class Memory(data: String):
-    private val pattern: Regex = "mul\\((\\d+),(\\d+)\\)".r
-    private val enable: Regex = "do\\(\\)".r
-    private val disable: Regex = "don't\\(\\)".r
-    def readInstruction(input: String = data): Long =
-      val matches = pattern.findAllMatchIn(input)
-      matches
-        .map:
-          case pattern(a, b) =>
-            a.toLong * b.toLong
-        .sum
-    def readInstructionWithConditional: Long =
-      val disabledIndex = disable
-        .findAllMatchIn(data)
-        .map: matched =>
-          matched.end + 1
-        .toSeq
-      val enabledIndex = enable
-        .findAllMatchIn(data)
-        .map: matched =>
-          matched.start
-        .toSeq
+  def partOne(input: String = aocInput): Long =
+    mulPattern
+      .findAllIn(input)
+      .map:
+        case mulPattern(a, b) =>
+          a.toLong * b.toLong
+      .sum
 
-      val newList = 0 +: enabledIndex
-      val result = newList
-        .zipAll(disabledIndex, 0, 0)
-        .flatMap:
-          case (a, b) if b == 0 => Seq(data.substring(a))
-          case (a, b) if b > a  => Seq(data.substring(a, b))
-          case (_, _)           => Nil
-        .map:
-          readInstruction
-
-      result.sum
+  def partTwo(input: String = aocInput): Long =
+    val (_, res2) = allPattern
+      .findAllIn(input)
+      .foldLeft((true, 0: Long)):
+        case ((true, sum), mulPattern(a, b)) =>
+          (true, sum + a.toLong * b.toLong)
+        case ((_, sum), "don't()") => (false, sum)
+        case ((_, sum), "do()")    => (true, sum)
+        case ((flag, sum), _)      => (flag, sum)
+    res2
 
   def main(args: Array[String]): Unit =
-    val memory = Memory(input.mkString)
-    val partOne: Long = memory.readInstruction()
-    println(s"Day 3 part one is $partOne")
-    val partTwo = memory.readInstructionWithConditional
-    println(s"Day 3 part two is $partTwo")
+    println(s"Day 3 part one is ${partOne()}")
+    println(s"Day 3 part two is ${partTwo()}")
 }
--- scala/src/test/scala/aoc24/Day3Test.scala
+++ scala/src/test/scala/aoc24/Day3Test.scala
@@ -1,19 +1,19 @@
 package dev.nheggoe
 package aoc24
 
-import aoc24.Day3.Memory
+import aoc24.Day3.{partOne, partTwo}
 
 class Day3Test extends munit.FunSuite:
+
   val exampleInput =
     "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-  val memory = Memory(exampleInput)
 
   test("Day 3 Part One") {
     val expected: Long = 161
-    assertEquals(memory.readInstruction(), expected)
+    assertEquals(partOne(exampleInput), expected)
   }
 
   test("Day 3 Part Two") {
     val expected: Long = 48
-    assertEquals(memory.readInstructionWithConditional, expected)
+    assertEquals(partTwo(exampleInput), expected)
   }
