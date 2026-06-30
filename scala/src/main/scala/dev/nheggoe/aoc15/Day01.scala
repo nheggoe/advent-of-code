6af8a9161088943a53a351a506adccc4b4034586
Nick Heggø <git@nheggoe.dev>
2026-06-30 18:30:21 +0200

    refactor: cleanup

--- scala/src/main/scala/dev/nheggoe/aoc15/Day01.scala
+++ scala/src/main/scala/dev/nheggoe/aoc15/Day01.scala
@@ -4,14 +4,13 @@ import dev.nheggoe.aoc.{AocDay, Input}
 
 object Day01 extends AocDay(1) {
 
-  def partOne(using Input): Int = {
+  override def partOne(using Input): Int =
     val (increase, decrease) = input.partition(_ == '(')
     increase.length - decrease.length
-  }
 
-  override def partTwo(using Input): Any =
-    val positions =
-      input.map(char => if char == '(' then 1 else -1).scan(0)(_ + _)
-    positions.takeWhile(_ != -1).size
+  override def partTwo(using Input): Int = {
+    for char <- input
+    yield if char == '(' then 1 else -1
+  }.scan(0)(_ + _).takeWhile(_ != -1).length
 
 }
--- scala/src/test/scala/dev/nheggoe/aoc15/Day01Test.scala
+++ scala/src/test/scala/dev/nheggoe/aoc15/Day01Test.scala
@@ -1,6 +1,8 @@
 package dev.nheggoe
 package aoc15
 
+import dev.nheggoe.aoc.Input
+import dev.nheggoe.aoc15.Day01.{partOne, partTwo}
 import munit.FunSuite
 
 class Day01Test extends FunSuite {
@@ -9,50 +11,50 @@ class Day01Test extends FunSuite {
     val inputOne = "(())"
     val inputTwo = "()()"
     val expected = 0
-    assertEquals(Day01.partOne(using inputOne), expected)
-    assertEquals(Day01.partOne(using inputTwo), expected)
+    assertEquals(partOne(using inputOne), expected)
+    assertEquals(partOne(using inputTwo), expected)
   }
 
   test("Part one case 2") {
     val inputOne = "((("
     val inputTwo = "(()(()("
     val expected = 3
-    assertEquals(Day01.partOne(using inputOne), expected)
-    assertEquals(Day01.partOne(using inputTwo), expected)
+    assertEquals(partOne(using inputOne), expected)
+    assertEquals(partOne(using inputTwo), expected)
   }
 
   test("Part one case 3") {
     val inputOne = "))((((("
     val expected = 3
-    assertEquals(Day01.partOne(using inputOne), expected)
+    assertEquals(partOne(using inputOne), expected)
   }
 
   test("Part one case 4") {
     val inputOne = "())"
     val inputTwo = "))("
     val expected = -1
-    assertEquals(Day01.partOne(using inputOne), expected)
-    assertEquals(Day01.partOne(using inputTwo), expected)
+    assertEquals(partOne(using inputOne), expected)
+    assertEquals(partOne(using inputTwo), expected)
   }
 
   test("Part one case 5") {
     val inputOne = ")))"
     val inputTwo = ")())())"
     val expected = -3
-    assertEquals(Day01.partOne(using inputOne), expected)
-    assertEquals(Day01.partOne(using inputTwo), expected)
+    assertEquals(partOne(using inputOne), expected)
+    assertEquals(partOne(using inputTwo), expected)
   }
 
   test("Part two case 1") {
     val input = ")"
     val expected = 1
-    assertEquals(Day01.partTwo(using input), expected)
+    assertEquals(partTwo(using input), expected)
   }
 
   test("Part two case 2") {
     val input = "()())"
     val expected = 5
-    assertEquals(Day01.partTwo(using input), expected)
+    assertEquals(partTwo(using input), expected)
   }
 
 }
